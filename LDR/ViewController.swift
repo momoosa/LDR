9c16764a9585764b85d516095ec28455ac78d555
Mo Moosa <momoosa@me.com>
2018-12-23 20:39:40 +0000

    Initial Commit

---
+++ LDR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LDR
+//
+//  Created by Mo Moosa on 23/12/2018.
+//  Copyright © 2018 Mo Moosa Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
